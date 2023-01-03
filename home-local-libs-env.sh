395060ab64da04cf368b176c7229fb270bae744a
Justin Ross <jross@apache.org>
2023-01-03 14:41:58 -0500

    WIP

---
+++ home-local-libs-env.sh
@@ -0,0 +1,5 @@
+export LIBRARY_PATH=$HOME/.local/lib64
+export C_INCLUDE_PATH=$HOME/.local/include
+export CPLUS_INCLUDE_PATH=$C_INCLUDE_PATH
+export LD_LIBRARY_PATH=$LIBRARY_PATH
+export PYTHONPATH=$(python -m site --user-site)
